05746fe540152388d12451f91a9385a25ba28ffc
Gus Narea <gnarea@users.noreply.github.com>
2022-07-06 12:11:48 +0100

    fix jest ci config

--- jest.config.ci.js
+++ jest.config.ci.js
@@ -3,6 +3,5 @@ const mainJestConfig = require('./jest.config');
 module.exports = Object.assign({}, mainJestConfig, {
   collectCoverageFrom: ["build/main/lib/**/*.js"],
   moduleFileExtensions: ['js'],
-  preset: null,
   roots: ['build/main/lib']
 });
